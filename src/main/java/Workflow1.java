87cf6769e8571cc7c5e7b02a87606c202c62c79d
rishikesh.mohanty <rishikesh.mohanty@ril.com>
2018-11-17 13:39:31 +0530

    changed workflow1

--- src/main/java/Workflow1.java
+++ src/main/java/Workflow1.java
@@ -2,4 +2,15 @@ public class Workflow1 {
 
   public String a = "ajkshdlkasd";
   public String b = "askhdasjhdasd";
+  public String c = "ajkshdlkasd";
+  public String d = "askhdasjhdasd";
+  public String e = "ajkshdlkasd";
+  public String f = "askhdasjhdasd";
+  public String g = "ajkshdlkasd";
+  public String h = "askhdasjhdasd";
+  public String i = "ajkshdlkasd";
+  public String j = "askhdasjhdasd";
+  public String k = "ajkshdlkasd";
+  public String l = "askhdasjhdasd";
+
 }
